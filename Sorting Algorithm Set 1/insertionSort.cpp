f2b2df8b1368d85cd22c64b53592abd38516f0f9
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-04-11 20:26:40 +0530

    added modified version of insertion sort using vector array

--- Sorting Algorithm Set 1/insertionSort.cpp
+++ Sorting Algorithm Set 1/insertionSort.cpp
@@ -3,19 +3,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+// Print function for displaying values
+void print_sort(vector<int> &arr){
+    for(int i = 0; i < arr.size(); i++){
+        cout<<arr[i]<<"\t";
+    }
+    cout<<"\n";
+}
 
-    int arr[] = {1, 3, 8, 2, 9, 2, 5, 6};
-    int n = 8, key = 0, j = 0;
-    for (int i = 1; i < n; i++) {
+// Sort function for performing insertion sort
+void insertion_sort(vector<int> &arr){
+    int key = 0, j = 0;
+    for (int i = 1; i < arr.size(); i++) {
         key = arr[i];
         for(j = i; ((j > 0) && (key < arr[j - 1])); j--){
             swap(arr[j], arr[j - 1]);
         }
         arr[j] = key;
     }
-    for(int i = 0; i < n; i++){
-        cout<<arr[i]<<"\t";
+}
+
+int main(){
+    // Insert testcase
+    int T;
+    cin>>T;
+
+    while(T){
+        // Initialize vector array
+        vector<int> arr;
+        int n = 0, temp_var = 0;
+        // Input size of array
+        cin>>n;
+        // Take user input values
+        for(int i = 0; i < n; i++){
+            cin>>temp_var;
+            arr.push_back(temp_var);
+        }
+
+        // Sorting function call
+        insertion_sort(arr);
+
+        // Printing function call
+        print_sort(arr);
+        T--;
     }
     return 0;
 }
