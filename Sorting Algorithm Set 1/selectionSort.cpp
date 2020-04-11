993fa83fb0eb0d9e9ef4ca3c770219ada8189dbc
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-04-11 20:33:07 +0530

    modified code with vector array

--- Sorting Algorithm Set 1/selectionSort.cpp
+++ Sorting Algorithm Set 1/selectionSort.cpp
@@ -3,21 +3,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+// Print function for displaying values
+void print_sort(vector<int> &arr){
+    for(auto i = arr.begin(); i != arr.end(); i++){
+        cout<<*i<<"\t";
+    }
+    cout<<"\n";
+}
 
-    int arr[] = {1, 3, 8, 2, 9, 2, 5, 6};
-    int n = 8, min = 0;
-    for (int i = 0; i < n; i++) {
+// Sort function for performing selection sort
+void selection_sort(vector<int> &arr){
+    int min = 0;
+    for (int i = 0; i < arr.size(); i++) {
         min = i;
-        for (int j = i + 1; j < n; j++) {
+        for (int j = i + 1; j < arr.size(); j++) {
             if (arr[j] < arr[min]) {
                 min = j;
             }
         }
-            swap(arr[min],arr[i]);
+        swap(arr[min],arr[i]);
     }
-    for(int i = 0; i < n; i++){
-        cout<<arr[i]<<"\t";
+}
+
+int main(){
+    // Insert testcase
+    int T;
+    cin>>T;
+
+    while(T){
+        // Initialize vector array
+        vector<int> arr;
+        int n = 0, temp_var = 0;
+        // Input size of array
+        cin>>n;
+        // Take user input values
+        for(int i = 0; i < n; i++){
+            cin>>temp_var;
+            arr.push_back(temp_var);
+        }
+
+        // Sorting function call
+        selection_sort(arr);
+
+        // Printing function call
+        print_sort(arr);
+        T--;
     }
     return 0;
 }
